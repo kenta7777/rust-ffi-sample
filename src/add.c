19bfafe47c77ae8985c912690a9120f5cc362c1d
kenta7777 <k.hasegw7@gmail.com>
2021-11-04 20:48:36 +0900

    add example of chaning the value of arguments

--- src/add.c
+++ src/add.c
@@ -1,3 +1,14 @@
 int add(int n1, int n2) {
     return n1 + n2;
 }
+
+int add_pointer(int* n1, int* n2) {
+    *n1 = 3;
+    *n2 = 4;
+    return *n1 + *n2;
+}
+
+void add_pointer_with_no_result(int* n1, int* n2) {
+    *n1 = 100;
+    *n2 = 100;
+}
